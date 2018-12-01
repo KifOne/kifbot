658dff001b1370a0c8c47c5cabf64494577ccbdb
KifOne <45518257+KifOne@users.noreply.github.com>
2018-12-01 20:04:38 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+token2 = "725414164:AAGl1x9jatHecYaedccDSEH5gXceE-Y-JVo"
+token = '640565821:AAH20mY_JIUmVv3nhttXLDUdMQjGDocFhRo'
+
+admin_id = 497221696
+
+town = ""
+
+region = ""
+
+tovar = ""
+
+volume = ""
+
+
+a = 286077227
+
+number = 0
